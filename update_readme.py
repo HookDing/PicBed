3498162e8f8f84b5a9e32243fe45bcd4bbbc551e
HookDing <80247717+HookDing@users.noreply.github.com>
2024-11-22 11:19:19 +0800

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -9,15 +9,12 @@ files = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path
 
 # 开始更新 README.md 文件
 with open(readme_path, 'w', encoding='utf-8') as readme_file:    
-    readme_file.write(" __  __                  __          __                          \n")
-    readme_file.write("/\ \/\ \                /\ \        /\ \  __                     \n")
-    readme_file.write("\ \ \_\ \    ___     ___\ \ \/'\    \_\ \/\_\    ___      __     \n")
-    readme_file.write(" \ \  _  \  / __`\  / __`\ \ , <    /'_` \/\ \ /' _ `\  /'_ `\   \n")
-    readme_file.write("  \ \ \ \ \/\ \L\ \/\ \L\ \ \ \\`\ /\ \L\ \ \ \/\ \/\ \/\ \L\ \  \n")
-    readme_file.write("   \ \_\ \_\ \____/\ \____/\ \_\ \_\ \___,_\ \_\ \_\ \_\ \____ \ \n")
-    readme_file.write("    \/_/\/_/\/___/  \/___/  \/_/\/_/\/__,_ /\/_/\/_/\/_/\/___L\ \\n")
-    readme_file.write("                                                          /\____/\n")
-    readme_file.write("                                                          \_/__/ \n")
+    readme_file.write("    __  __            __       ___            \n")
+    readme_file.write("   / / / /___  ____  / /______/ (_)___  ____ _\n")
+    readme_file.write("  / /_/ / __ \/ __ \/ //_/ __  / / __ \/ __ `/\n")
+    readme_file.write(" / __  / /_/ / /_/ / ,< / /_/ / / / / / /_/ / \n")
+    readme_file.write("/_/ /_/\____/\____/_/|_|\__,_/_/_/ /_/\__, /  \n")
+    readme_file.write("                                     /____/   \n")
     # readme_file.write("# A Repository for PicGO\n\n")
     readme_file.write("## img contents\n\n")
     for filename in files:
