0fe1b7a759522ead5d4ee7ef2e31dad05b52a3ca
HookDing <80247717+HookDing@users.noreply.github.com>
2024-11-22 11:23:22 +0800

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -9,12 +9,16 @@ files = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path
 
 # 开始更新 README.md 文件
 with open(readme_path, 'w', encoding='utf-8') as readme_file:    
+    # 添加 ASCII 艺术和标题
+    readme_file.write("\n")
+    readme_file.write("```\n")
     readme_file.write("    __  __            __       ___            \n")
     readme_file.write("   / / / /___  ____  / /______/ (_)___  ____ _\n")
     readme_file.write("  / /_/ / __ \/ __ \/ //_/ __  / / __ \/ __ `/\n")
     readme_file.write(" / __  / /_/ / /_/ / ,< / /_/ / / / / / /_/ / \n")
     readme_file.write("/_/ /_/\____/\____/_/|_|\__,_/_/_/ /_/\__, /  \n")
     readme_file.write("                                     /____/   \n")
+    readme_file.write("```\n\n")
     # readme_file.write("# A Repository for PicGO\n\n")
     readme_file.write("## img contents\n\n")
     for filename in files:
